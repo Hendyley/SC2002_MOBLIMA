c5b2e5bbf6b50e768f2faad43c8d11e0234743a7
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2022-11-01 10:49:02 +0800

    Added some enum

---
+++ src/main/java/MovieGoerModule/Day.java
@@ -0,0 +1,5 @@
+package MovieGoerModule;
+
+public enum Day {
+    MON_TO_WED, THURS, FRI_BEFORE_6, REMAINING_DAYS
+}
--- src/main/java/MovieGoerModule/Ticket.java
+++ src/main/java/MovieGoerModule/Ticket.java
@@ -3,4 +3,23 @@ package MovieGoerModule;
 public class Ticket {
     private int quantity;
     private AgeOfMovieGoer age;
+    private TypeOfMovie movieType;
+    private double price;
+    private ClassOfCinama cinemaClass;
+
+    public Ticket() {
+
+    }
+
+    public Ticket(int quantity, AgeOfMovieGoer age, TypeOfMovie movieType, ClassOfCinama cinemaClass) {
+        this.quantity = quantity;
+        this.age = age;
+        this.movieType = movieType;
+        this.cinemaClass = cinemaClass;
+    }
+
+    public double calcPrice() {
+
+    }
+
 }
