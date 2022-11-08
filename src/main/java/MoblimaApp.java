61180234cf6e631a59d0856a51c0cd65ceeecbc1
Lee Chun Yang <40287858+leechunyang98@users.noreply.github.com>
2022-11-08 20:52:28 +0800

    added moviegoer case 1,2,3,8 to MoblimaApp

--- src/main/java/MoblimaApp.java
+++ src/main/java/MoblimaApp.java
@@ -1,17 +1,27 @@
 import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Scanner;
+
+import MovieGoerModule.Cineplex;
 import MovieGoerModule.Movie;
 import MovieGoerModule.MovieGoer;
+import MovieGoerModule.Review;
 import MovieGoerModule.Role;
+import MovieGoerModule.TimeSlot;
 import AdminModule.Staff;
+import AdminModule.cineplexDB;
 
 public class MoblimaApp {
     private final static String ACCOUNT_FILE_NAME = "AccountList.txt";
+    private final static String CINEPLEX_FILE_NAME = "Cineplex.txt";
+    private final static String PRICELIST_FILE_NAME = "Pricelist.txt";
     private static Scanner sc = new Scanner(System.in);
     private static Role currentRole;
     private static boolean isLogined = false;
+    private static boolean isGuest = false;
     public String[] holidays = { "01/01/2022", "01/02/2022", "02/02/2022", "15/04/2022", "01/05/2022", "03/05/2022",
     "15/05/2022", "10/07/2022", "09/08/2022", "24/10/2022", "25/12/2022" };
     public static void main(String[] args) throws ClassNotFoundException, IOException{
@@ -35,7 +45,8 @@ public class MoblimaApp {
                     switch_case_login();
                     break;
                 case 2:
-                    switch_case_guest();
+                    isGuest = true;
+                    switch_case_moviegoer();
                     break;
                 default:
                     break;
@@ -204,84 +215,264 @@ public class MoblimaApp {
 
     }
 
-    private static void switch_case_guest(){
-        System.out.println("switch_case_guest");
-    }
 
     private static void switch_case_admin(){
         System.out.println("switch_case_admin");
     }
 
-    private static void switch_case_moviegoer(){
-        // System.out.println("switch_case_moviegoer");
-            // Initialisation
-            Calendar today = Calendar.getInstance();
+    private static void switch_case_moviegoer() throws ClassNotFoundException, IOException{
+        ArrayList<Cineplex> cathay = cineplexDB.getCineplexListFromFile();
+        Scanner sc = new Scanner(System.in);
 
-            // Cineplex cathay = new Cineplex(3);
-            // TimeSlot newSlot = new TimeSlot("02/11/2022", 1700);
-    
-            String Date1 = "31/10/2022";
-    
-            Movie movie1 = new Movie("Batman");
-            Movie movie2 = new Movie("Joker");
-            Movie movie3 = new Movie("Superman");
-    
-            Movie[] movieArr = new Movie[3];
-    
-            // *************************
-            Scanner sc = new Scanner(System.in);
-    
+
+
+        int option = 0, cinema = 0, choice = 0, Qty = 0;
+        float time;
+        String key, datetime, movie, seatdesc;
+        Cineplex cinename;
+        ArrayList<Movie> movieList;
+        Scanner s = new Scanner(System.in);
+        do {
             System.out.println("********************");
             System.out.println("Movie Goer Module");
             System.out.println("1. Search/List movie");
             System.out.println("2. View Movie details");
             System.out.println("3. Seat Availability and Booking");
-            System.out.println("4. View Booking History");
-            System.out.println("5. List Top 5 Movies by sales OR by overall ratings");
-            System.out.println("6. Exit");
+            System.out.println("4. Book a ticket");
+            System.out.println("5. View Booking History");
+            System.out.println("6. List Top 5 Movies by sales OR by overall ratings");
+    
+            System.out.println("7. Give a movie review");
+            if(isLogined){
+                System.out.println("8. Log out");
+            }else{
+                System.out.println("8. End Guest Session");
+            }
             System.out.println("********************");
-    
-            int option = 0;
-            do {
-                System.out.println("Enter Option");
-                option = sc.nextInt();
-    
-                switch (option) {
-                    case 1:
-                        System.out.println("1. Search/List movie");
-    
-                        break;
-                    case 2:
-                        System.out.println("2. View Movie details");
-    
-                        break;
-                    case 3:
-                        System.out.println("3. Seat Availability and Booking");
-                        Calendar bookingDay = Calendar.getInstance();
-                        bookingDay.set(2022, 10, 25); //
-                        System.out.println("Which Day?");
-                        System.out.println("Which Movie?");
-                        System.out.println("Which time slot?");
-    
-                        break;
-                    case 4:
-                        System.out.println("4. View Booking History");
-    
-                        break;
-    
-                    case 5:
-                        System.out.println("5. List Top 5 Movies by sales OR by overall ratings");
-    
-                        break;
 
-                    case 6:
+            System.out.println("Enter Option");
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("1. Search/List movie");
+                   
+                    movieList = getMovieList(cathay,sc);
+                    do {
+                        boolean isNotFound = true;
+                        System.out.println("1. Search movie");
+                        System.out.println("2. List movie");
+                        System.out.println("3. Exit");
+                        System.out.println("Enter a choice");
+                        choice = sc.nextInt();
+                         //clear buffer
+                         sc.nextLine();
+
+                        switch (choice) {
+                            case 1:
+                                System.out.println("Search by keyword: ");
+                                key = s.nextLine();
+                                System.out.println("Movie search results:");
+                                for (int j = 0; j < movieList.size(); j++) {
+                                    if (movieList.get(j).getTitle().toLowerCase().indexOf(key.toLowerCase()) != -1) {
+                                        System.out.println(j + ". " + movieList.get(j).getTitle());
+                                        isNotFound = false;
+                                    }
+                                }
+                                break;
+                            
+                            case 2:
+                                System.out.println("Movie lists:");
+                                for (int j = 0; j < movieList.size(); j++) {
+                                    System.out.println(j + ". " + movieList.get(j).getTitle());
+                                    isNotFound = false;
+                                }
+                                break;
+                            case 3:
+                                System.out.println("Exiting Search/List movie...");
+                                break;
+
+                            default:
+                                break;
+                        }
+                        
+                        if(isNotFound  && (choice == 1 || choice == 2)){
+                            System.out.println("No result found");
+                        }
+                        System.out.println("");                        
+                    } while (choice != 3);
+
+                    break;
+                case 2:
+                    System.out.println("2. View Movie details");
+                    
+                    movieList = getMovieList(cathay,sc);
+                    if(movieList.size() < 1){
+                        System.out.println("No movie available.");
+                        break;
+                    }
+
+                    for (int i = 0; i < movieList.size(); i++) {
+                        System.out.println(i + " Movie " + movieList.get(i).getTitle()+" "+ movieList.get(i).getStatus()+" "
+                            +movieList.get(i).getType()+" "+movieList.get(i).getAge_restriction());
+                    }
+                    System.out.println("Select movie to view details");
+                    choice = sc.nextInt();
+                    if(choice>=movieList.size()){
+                        System.out.println("Please Choose appropriate Movie!");
+                        break;
+                    }
+                    System.out.println("Movie Details : ");
+                    System.out.println("Movie Title: " + movieList.get(choice).getTitle());
+                    System.out.println("Movie Status: " + movieList.get(choice).getStatus());
+                    System.out.println("Movie Duration: "+ movieList.get(choice).getMovieDurationMin()+ " minutes");
+                    System.out.println("Movie Age Requirement: "+ movieList.get(choice).getAge_restriction());
+                    System.out.println("Movie Type: "+ movieList.get(choice).getType());
+                    System.out.println("Movie Rating: " + movieList.get(choice).getRating());
+                    System.out.println("Movie Director: " + movieList.get(choice).getDirector());
+                    System.out.println("Movie Synopsis: " + movieList.get(choice).getSynopsis());
+                    System.out.println("Movie Airing time: ");
+                    ArrayList<TimeSlot> showair = movieList.get(choice).getTimeSlots();
+                    for(int i=0; i<showair.size(); i++){
+                        System.out.println(showair.get(i).getairingtimeformat()+" "+showair.get(i).getRoom().getCinemaClass());
+                    }
+                    System.out.println("Movie Review: ");
+                    ArrayList<Review> showreview = movieList.get(choice).getreviewlist();
+                    for(int i=0; i<showreview.size(); i++){
+                        System.out.println("Comment: "+i+" "+showreview.get(i).getRemark());
+                    }
+                    System.out.println("");
+                    break;
+
+                case 3:
+                    ArrayList<String> dateList = new ArrayList<String>();
+                    ArrayList<TimeSlot> slotList = new ArrayList<TimeSlot>();
+                    ArrayList<TimeSlot> slotList2 = new ArrayList<TimeSlot>();
+                    String dateSelection = "";
+                    TimeSlot slotSelected;
+
+                    System.out.println("3. Seat Availability and Booking");
+                    System.out.println("Cineplex List:");
+                    for (int i = 0; i < cathay.size(); i++) {
+                        System.out.println(i + ". " + cathay.get(i).getName());
+                    }
+                    System.out.println("Select one of the cineplex index");
+                    cinema = sc.nextInt();
+                    if (cinema >= cathay.size()) {
+                        System.out.println("Please Choose appropriate Cinema!");
+                        break;
+                    }
+                    cinename = cathay.get(cinema);
+                    // if (cinename.getTimeslots().size() == 0) {
+                    // System.out.println("No Movie is airing in this cineplex");
+                    // break;
+                    // }
+                    if(cinename.getMovieList().size() < 1){
+                        System.out.println("No movie available");
+                        break;
+                    }
+                    System.out.println("Which movie :");
+
+                    for (int i = 0; i < cinename.getMovieList().size(); i++) {
+                        System.out.println(i + " " + cinename.getMovieList().get(i).getTitle());
+                    }
+                    int movieSelection = sc.nextInt();
+                    if (movieSelection >= cinename.getMovieList().size()) {
+                        System.out.println("Please Choose appropriate Movie!");
+                        break;
+                    }
+                    slotList = cinename.getMovieList().get(movieSelection).getTimeSlots();
+
+                    if (slotList.size() == 0) {
+                        System.out.println("No date Available");
+                    } else {
+                        System.out.println("Select a date");
+                        String firstDate = slotList.get(0).getStringDate();
+                        System.out.println(0 + " " + slotList.get(0).getStringDate());
+                        dateList.add(firstDate);
+                        for (int i = 1; i < slotList.size(); i++) {
+                            if (slotList.get(i - 1).getStringDate() != slotList.get(i).getStringDate()) {
+                                dateList.add(slotList.get(i).getStringDate());
+                                System.out.println(i + " " + slotList.get(i).getStringDate());
+                            }
+                        }
+
+                        int input = sc.nextInt();
+                        if (input >= dateList.size()) {
+                            System.out.println("Please Choose appropriate Date!");
+                            break;
+                        }
+                        dateSelection = dateList.get(input);
+                    }
+
+                    System.out.println("Select Timeslot");
+                    for (int i = 0; i < slotList.size(); i++) {
+                        if (slotList.get(i).getStringDate() == dateSelection) {
+                            slotList2.add(slotList.get(i));
+                        }
+                    }
+                    for (int i = 0; i < slotList2.size(); i++) {
+                        System.out.println(
+                                i + " " + slotList2.get(i).getStartTime() + "-" +
+                                        slotList2.get(i).getEndTime());
+                    }
+
+                    int inputSlot = sc.nextInt();
+                    if (inputSlot >= slotList2.size()) {
+                        System.out.println("Please Choose appropriate TimeSlot!");
+                        break;
+                    }
+                    slotSelected = slotList2.get(inputSlot);
+                    slotSelected.getRoom().printSeats();
+
+                    seatdesc = slotSelected.getRoom().getseattypedesc();
+                    String couples = seatdesc.substring(0,seatdesc.indexOf("Elite"));
+                    String elite = seatdesc.substring(seatdesc.indexOf("Elite"),seatdesc.indexOf("Ulti"));
+                    String ulti = seatdesc.substring(seatdesc.indexOf("Ulti"));
+                    System.out.println(couples);
+                    System.out.println(elite);
+                    System.out.println(ulti);
+
+                    break;
+                case 8:
+                    if(isLogined){
                         isLogined = false;
                         System.out.println("Logged Out Successfully!");
-                    default:
-                        break;
-                }
-    
-            } while (isLogined);
+                    }
+
+                    if(isGuest){
+                        isGuest = false;
+                        System.out.println("Guest Session Ended");
+                    }
+                    break;
+                    
+                default:
+                    break;
+            }
+
+        } while (isLogined || isGuest);
+
+    }
+
+
+    private static ArrayList<Movie> getMovieList(ArrayList<Cineplex> cathay, Scanner sc){
+        int i = 0;
+        int index = -1;
+        ArrayList<Movie> movieList;
+
+        System.out.println("Cineplex List:");
+        for(Cineplex cineplex :cathay){
+            System.out.printf("%s. %s\n",i,cineplex.getName());
+            i++;
+        }
+        
+        do {
+            System.out.println("Select one of the cineplex index");
+            index = sc.nextInt();
+        } while (index < 0 || index > cathay.size()-1);
+       
+
+       return movieList = cathay.get(index).getMovieList();
     }
 
 
