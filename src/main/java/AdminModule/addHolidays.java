c294cdb686e312a1151cb7aad3f7757c9060d513
Qi <lim.qile@yahoo.com.sg>
2022-11-09 13:10:38 +0800

    fixed error in addHoliday

--- src/main/java/AdminModule/addHolidays.java
+++ src/main/java/AdminModule/addHolidays.java
@@ -7,10 +7,10 @@ public class addHolidays {
     public void addDay(String date){
         String filename = "holidays.txt";
         try{
-            ArrayList al = holidayTextDB.readHolidays(filename);
+            ArrayList al = holidayTextDB.readHolidays();
             Holiday d = new Holiday(date);
             al.add(d);
-            holidayTextDB.saveHoliday(filename, al);
+            holidayTextDB.saveHoliday(al);
         } catch(Exception e){ 
             System.out.println("IOException > " + e.getMessage());
         }
